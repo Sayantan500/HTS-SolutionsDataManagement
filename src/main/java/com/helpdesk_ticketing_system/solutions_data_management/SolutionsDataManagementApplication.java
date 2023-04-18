d183db971013299b5b084523a377821f30dd7478
sayantan <sayantandas657@outlook.com>
2023-04-18 20:47:20 +0530

    Initial Commit

---
+++ src/main/java/com/helpdesk_ticketing_system/solutions_data_management/SolutionsDataManagementApplication.java
@@ -0,0 +1,13 @@
+package com.helpdesk_ticketing_system.solutions_data_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionsDataManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionsDataManagementApplication.class, args);
+	}
+
+}
