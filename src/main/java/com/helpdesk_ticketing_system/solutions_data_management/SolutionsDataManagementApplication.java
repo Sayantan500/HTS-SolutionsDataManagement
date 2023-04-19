ce4474a027d3c8e42023283d570ebfcb03982d3a
sayantan <sayantandas657@outlook.com>
2023-04-19 14:50:49 +0530

    Changed tomcat port

--- src/main/java/com/helpdesk_ticketing_system/solutions_data_management/SolutionsDataManagementApplication.java
+++ src/main/java/com/helpdesk_ticketing_system/solutions_data_management/SolutionsDataManagementApplication.java
@@ -3,11 +3,16 @@ package com.helpdesk_ticketing_system.solutions_data_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SolutionsDataManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SolutionsDataManagementApplication.class, args);
+		SpringApplication application = new SpringApplication(SolutionsDataManagementApplication.class);
+		application.setDefaultProperties(Collections
+				.singletonMap("server.port", System.getenv("tomcat_server_port")));
+		application.run(args);
 	}
 
 }
