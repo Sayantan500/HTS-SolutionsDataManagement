a3c63c3f6a5121fc8420a9ed6a95dfc7e70c6f83
sayantan <sayantandas657@outlook.com>
2023-04-19 20:42:41 +0530

    Updated solutionDocument class to specify the field names for mongodb

--- src/main/java/com/helpdesk_ticketing_system/solutions_data_management/entities/SolutionDocument.java
+++ src/main/java/com/helpdesk_ticketing_system/solutions_data_management/entities/SolutionDocument.java
@@ -1,16 +1,23 @@
 package com.helpdesk_ticketing_system.solutions_data_management.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+@Document
 public class SolutionDocument {
     @JsonProperty("solution_id") @MongoId(value = FieldType.STRING)
     private String _id;
-    @JsonProperty("ticket_id") private String ticketId;
-    @JsonProperty("issue_id") private String issueId;
-    @JsonProperty("solution") private Solution solution;
-    @JsonProperty("posted_on") private Long postedOn;
+    @JsonProperty("ticket_id") @Field(name = "ticket_id")
+    private String ticketId;
+    @JsonProperty("issue_id") @Field(name = "issue_id")
+    private String issueId;
+    @JsonProperty("solution") @Field(name = "solution")
+    private Solution solution;
+    @JsonProperty("posted_on") @Field(name = "posted_on")
+    private Long postedOn;
 
     public SolutionDocument() {
     }
